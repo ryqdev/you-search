ddfaf7f22e096e4a0b861bf965694d7e15f5a4ec
Yukun Shang <shangyukun.ryan@bytedance.com>
2024-01-14 22:28:19 +0800

    add

--- api/index.py
+++ api/index.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, request
+import json
+from youtube_transcript_api import YouTubeTranscriptApi
 
 app = Flask(__name__)
 
@@ -8,8 +10,28 @@ def home():
 
 @app.route('/about')
 def about():
-    return 'About123'
+    return 'About'
 
-@app.route('/tldr')
+@app.route('/tldr', methods = ['POST', 'GET'])
 def tldr():
-    return 'tldr'
+    if request.method == 'GET':
+        return "tldr"
+    else:
+        json_str = request.body.decode('utf-8')
+        json_data = json.loads(json_str)
+        video_id = json_data['video_id']
+        subtitles = json.dumps(get_subtitles(video_id))
+        return subtitles 
+
+def get_subtitles(video_id):
+    # video_id = extract_video_id(url)
+
+    transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
+
+    # iterate over all available transcripts
+    for transcript in transcript_list:
+        result = transcript.fetch()
+        if result is not None:
+            return result
+
+    return ""
