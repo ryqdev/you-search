e381a92f17aff5bf5ca8071984d811f54948a509
Yukun Shang <shangyukun.ryan@bytedance.com>
2024-01-14 21:51:44 +0800

    init

--- api/index.py
+++ api/index.py
@@ -1,6 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/api/python")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+@app.route('/about')
+def about():
+    return 'About'
